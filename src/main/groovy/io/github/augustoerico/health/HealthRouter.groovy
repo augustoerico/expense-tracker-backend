cb41022fda91e6d4d61f283676780f68eb681edb
Erico Silva <augusto.ericosilva@gmail.com>
2017-03-29 23:04:20 -0300

    Setup project

---
+++ src/main/groovy/io/github/augustoerico/health/HealthRouter.groovy
@@ -0,0 +1,22 @@
+package io.github.augustoerico.health
+
+import io.vertx.core.http.HttpMethod
+import io.vertx.ext.web.Router
+
+class HealthRouter {
+
+    Router router
+
+    HealthRouter(Router router) {
+        this.router = router
+    }
+
+    def route() {
+        router.route(HttpMethod.GET, '/health').handler GetHealthHandler.handler
+    }
+
+    static create(Router router) {
+        new HealthRouter(router)
+    }
+
+}
