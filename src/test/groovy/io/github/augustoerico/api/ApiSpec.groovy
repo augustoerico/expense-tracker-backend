bba7a2e5b95375fa56a02b2229e5228a6a813330
Erico Silva <augusto.ericosilva@gmail.com>
2017-04-01 16:33:53 -0300

    Add base class for API specs

---
+++ src/test/groovy/io/github/augustoerico/api/ApiSpec.groovy
@@ -0,0 +1,49 @@
+package io.github.augustoerico.api
+
+import de.flapdoodle.embed.mongo.MongodExecutable
+import groovyx.net.http.RESTClient
+import io.github.augustoerico.config.Env
+import io.github.augustoerico.db.Repository
+import io.vertx.core.Vertx
+import spock.lang.Shared
+import spock.lang.Specification
+
+class ApiSpec extends Specification {
+
+    @Shared
+    Vertx vertx
+    @Shared
+    MongodExecutable executable
+
+    @Shared
+    RESTClient restClient
+    @Shared
+    Repository repository
+
+    def setupSpec() {
+        vertx = Vertx.vertx()
+
+        TestHelper.setupServer(vertx)
+
+        executable = TestHelper.getMongodExecutable()
+        executable.start()
+
+        repository = Repository.create(vertx).getInstance()
+
+        restClient = new RESTClient(Env.appUrl())
+        restClient.setContentType('application/json')
+
+        setupContextSpec()
+    }
+
+    def cleanupSpec() {
+        executable.stop()
+        vertx.close()
+    }
+
+    /**
+     * Override to add behavior to setupSpec
+     */
+    def setupContextSpec() { }
+
+}
