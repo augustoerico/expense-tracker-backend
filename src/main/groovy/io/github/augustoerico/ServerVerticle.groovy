07b504aa7696b6b50fb2cf7f81aa0625f03791e5
Erico Silva <augusto.ericosilva@gmail.com>
2017-04-02 00:29:53 -0300

    Add logger to server

--- src/main/groovy/io/github/augustoerico/ServerVerticle.groovy
+++ src/main/groovy/io/github/augustoerico/ServerVerticle.groovy
@@ -2,16 +2,22 @@ package io.github.augustoerico
 
 import io.github.augustoerico.auth.AuthRouter
 import io.github.augustoerico.config.Env
+import io.github.augustoerico.expenses.ExpensesRouter
 import io.github.augustoerico.health.HealthRouter
 
 import io.vertx.core.AbstractVerticle
 import io.vertx.core.Future
 import io.vertx.core.http.HttpMethod
+import io.vertx.core.logging.Logger
+import io.vertx.core.logging.LoggerFactory
 import io.vertx.ext.auth.jwt.JWTAuth
 import io.vertx.ext.web.Router
+import io.vertx.ext.web.handler.BodyHandler
 import io.vertx.ext.web.handler.CorsHandler
 
 class ServerVerticle extends AbstractVerticle {
+
+    static final Logger LOGGER = LoggerFactory.getLogger ServerVerticle
 
     Router router
 
@@ -33,22 +39,25 @@ class ServerVerticle extends AbstractVerticle {
                 .listen(Env.port(), Env.address(), handleResult.curry(future))
     }
 
-    def handleResult = { Future future, result ->
+    def handleResult = { Future future, Future result ->
         if (result.succeeded()) {
-            println "Server running on http://${Env.address()}:${Env.port()}"
+            LOGGER.info "Server running on http://${Env.address()}:${Env.port()}"
             future.complete()
         } else {
             def ex = result.cause()
-            ex.printStackTrace()
+            LOGGER.error ex.message, ex
             future.fail(ex)
         }
     }
 
     def registerAppRoutes() {
+        router.route().handler BodyHandler.create()
+
         def authProvider = JWTAuth.create(vertx, Env.authProviderConfig())
 
         HealthRouter.create(router).route()
         AuthRouter.create(router, authProvider).route()
+        ExpensesRouter.create(router, authProvider).route()
     }
 
 }
