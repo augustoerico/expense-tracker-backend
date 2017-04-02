2f624630d5510c47e988e699785be915a2bb0816
Erico Silva <augusto.ericosilva@gmail.com>
2017-04-02 11:36:34 -0300

    Add admin router; add list handler

---
+++ src/main/groovy/io/github/augustoerico/admin/AdminRouter.groovy
@@ -0,0 +1,29 @@
+package io.github.augustoerico.admin
+
+import io.vertx.ext.auth.jwt.JWTAuth
+import io.vertx.ext.web.Router
+import io.vertx.ext.web.handler.JWTAuthHandler
+
+class AdminRouter {
+
+    Router router
+    JWTAuth jwtAuthProvider
+
+    private AdminRouter(Router router, JWTAuth jwtAuthProvider) {
+        this.router = router
+        this.jwtAuthProvider = jwtAuthProvider
+    }
+
+    static create(Router router, JWTAuth jwtAuthProvider) {
+        new AdminRouter(router, jwtAuthProvider)
+    }
+
+    def route() {
+        router.route('/admin/*')
+                .handler JWTAuthHandler.create(jwtAuthProvider)
+                    .addAuthority('admin').&handle
+
+        router.get('/admin/expenses')
+    }
+
+}
---
+++ src/main/groovy/io/github/augustoerico/admin/handlers/AdminListExpensesHandler.groovy
@@ -0,0 +1,40 @@
+package io.github.augustoerico.admin.handlers
+
+import io.github.augustoerico.config.Env
+import io.github.augustoerico.db.Repository
+import io.vertx.core.Future
+import io.vertx.core.http.HttpServerResponse
+import io.vertx.core.logging.Logger
+import io.vertx.core.logging.LoggerFactory
+import io.vertx.ext.web.RoutingContext
+
+class AdminListExpensesHandler {
+
+    static final Logger LOGGER = LoggerFactory.getLogger AdminListExpensesHandler
+
+    HttpServerResponse response
+
+    static create() {
+        new AdminListExpensesHandler()
+    }
+
+    def handle = { RoutingContext context ->
+        LOGGER.info "[GET] ${context.normalisedPath()}"
+
+        response = context.response()
+
+        Repository.create(context.vertx()).find(Env.expensesCollection(),
+                handleResult)
+    }
+
+    def handleResult = { Future future ->
+        if (future.succeeded()) {
+            def result = future.result()*.encodePrettily()
+            response.setStatusCode(200).end(result)
+        } else {
+            def ex = future.cause()
+            LOGGER.error ex.message, ex
+            response.setStatusCode(500).end(ex.message)
+        }
+    }
+}
